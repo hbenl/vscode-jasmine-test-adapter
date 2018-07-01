8c02755bc5cc98bc42b12c4ff704c30e2accb216
Florent Vilmart <florent@flovilmart.com>
2018-07-01 08:58:33 -0400

    Adds ability to decorate test failures

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,38 @@
 import * as vscode from 'vscode';
-import { TestExplorerExtension, testExplorerExtensionId } from 'vscode-test-adapter-api';
+import { TestExplorerExtension, testExplorerExtensionId, TestEvent } from 'vscode-test-adapter-api';
 import { JasmineAdapter } from './adapter';
+import { TestDecorationsManager } from './decorationsManager';
+
+function getWorkspaceRegistrar(
+	testExplorerExtension:vscode.Extension<TestExplorerExtension>,
+	context: vscode.ExtensionContext) {
+
+	const registeredAdapters = new Map<vscode.WorkspaceFolder, JasmineAdapter>();
+
+	function add(workspaces: vscode.WorkspaceFolder[]) {
+		for (const workspaceFolder of workspaces) {
+			const adapter = new JasmineAdapter(workspaceFolder);
+			const manager = new TestDecorationsManager(workspaceFolder.uri.fsPath, context);
+			adapter.testStates((event) => 
+				manager.handle(event as TestEvent)
+			, null, context.subscriptions);
+			registeredAdapters.set(workspaceFolder, adapter);
+			testExplorerExtension.exports.registerAdapter(adapter);
+		}
+	}
+
+	function remove(workspaces: vscode.WorkspaceFolder[]) {
+		for (const workspaceFolder of workspaces) {
+			const adapter = registeredAdapters.get(workspaceFolder);
+			if (adapter) {
+				testExplorerExtension.exports.unregisterAdapter(adapter);
+				registeredAdapters.delete(workspaceFolder);
+			}
+		}
+	}
+
+	return { add, remove };
+}
 
 export async function activate(context: vscode.ExtensionContext) {
 
@@ -12,31 +44,15 @@ export async function activate(context: vscode.ExtensionContext) {
 			await testExplorerExtension.activate();
 		}
 
-		const registeredAdapters = new Map<vscode.WorkspaceFolder, JasmineAdapter>();
+		const { add, remove } = getWorkspaceRegistrar(testExplorerExtension, context);
 
 		if (vscode.workspace.workspaceFolders) {
-			for (const workspaceFolder of vscode.workspace.workspaceFolders) {
-				const adapter = new JasmineAdapter(workspaceFolder);
-				registeredAdapters.set(workspaceFolder, adapter);
-				testExplorerExtension.exports.registerAdapter(adapter);
-			}
+			add(vscode.workspace.workspaceFolders);
 		}
 	
 		vscode.workspace.onDidChangeWorkspaceFolders((event) => {
-	
-			for (const workspaceFolder of event.removed) {
-				const adapter = registeredAdapters.get(workspaceFolder);
-				if (adapter) {
-					testExplorerExtension.exports.unregisterAdapter(adapter);
-					registeredAdapters.delete(workspaceFolder);
-				}
-			}
-	
-			for (const workspaceFolder of event.added) {
-				const adapter = new JasmineAdapter(workspaceFolder);
-				registeredAdapters.set(workspaceFolder, adapter);
-				testExplorerExtension.exports.registerAdapter(adapter);
-			}
+			remove(event.removed);
+			add(event.added);
 		});
 	}
 }
