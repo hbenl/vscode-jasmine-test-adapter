4b38320559462047f009019c982cae9d1d42d01f
Holger Benl <hbenl@evandor.de>
2018-07-12 15:08:40 +0200

    use the Registrar from the util package

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,12 @@
 import * as vscode from 'vscode';
 import { TestExplorerExtension, testExplorerExtensionId } from 'vscode-test-adapter-api';
-import { Log } from 'vscode-test-adapter-util';
+import { Log, TestAdapterRegistrar } from 'vscode-test-adapter-util';
 import { JasmineAdapter } from './adapter';
-
-function getWorkspaceRegistrar(
-	testExplorerExtension:vscode.Extension<TestExplorerExtension>,
-	log: Log
-) {
-
-	const registeredAdapters = new Map<vscode.WorkspaceFolder, JasmineAdapter>();
-	const channel = vscode.window.createOutputChannel('Jasmine Tests');
-
-	function add(workspaces: vscode.WorkspaceFolder[]) {
-		for (const workspaceFolder of workspaces) {
-			const adapter = new JasmineAdapter(workspaceFolder, channel, log);
-			registeredAdapters.set(workspaceFolder, adapter);
-			testExplorerExtension.exports.registerAdapter(adapter);
-		}
-	}
-
-	function remove(workspaces: vscode.WorkspaceFolder[]) {
-		for (const workspaceFolder of workspaces) {
-			const adapter = registeredAdapters.get(workspaceFolder);
-			if (adapter) {
-				testExplorerExtension.exports.unregisterAdapter(adapter);
-				registeredAdapters.delete(workspaceFolder);
-				adapter.dispose();
-			}
-		}
-	}
-
-	return { add, remove };
-}
 
 export async function activate(context: vscode.ExtensionContext) {
 
 	const workspaceFolder = (vscode.workspace.workspaceFolders || [])[0];
+	const channel = vscode.window.createOutputChannel('Jasmine Tests');
 	const log = new Log('jasmineExplorer', workspaceFolder, 'Jasmine Explorer Log');
 
 	const testExplorerExtension = vscode.extensions.getExtension<TestExplorerExtension>(testExplorerExtensionId);
@@ -48,15 +19,10 @@ export async function activate(context: vscode.ExtensionContext) {
 			await testExplorerExtension.activate();
 		}
 
-		const { add, remove } = getWorkspaceRegistrar(testExplorerExtension, log);
-
-		if (vscode.workspace.workspaceFolders) {
-			add(vscode.workspace.workspaceFolders);
-		}
-	
-		vscode.workspace.onDidChangeWorkspaceFolders((event) => {
-			remove(event.removed);
-			add(event.added);
-		});
+		context.subscriptions.push(new TestAdapterRegistrar(
+			testExplorerExtension.exports,
+			(workspaceFolder) => new JasmineAdapter(workspaceFolder, channel, log),
+			log
+		));
 	}
 }
